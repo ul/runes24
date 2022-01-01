d26bf232ad971912ec236625063ca8362b7605da
Ruslan Prakapchuk <fer.obbee@gmail.com>
2022-01-01 12:57:14 +1100

    compress state and make new icons

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,7 +4,8 @@
 )]
 
 use std::{
-    fs::{create_dir_all, read_to_string, write},
+    fs::{create_dir_all, File},
+    io::{Read, Write},
     path::PathBuf,
 };
 
@@ -12,18 +13,25 @@ fn get_state_path() -> PathBuf {
     let mut state_path = dirs::home_dir().unwrap();
     state_path.push(".runes24");
     create_dir_all(&state_path).unwrap();
-    state_path.push("state.json");
+    state_path.push("state.json.snap");
     state_path
 }
 
 #[tauri::command]
 fn get_initial_state() -> Option<String> {
-    read_to_string(get_state_path()).ok()
+    let file = File::open(get_state_path()).ok()?;
+    let mut rdr = snap::read::FrameDecoder::new(file);
+    let mut string = String::new();
+    rdr.read_to_string(&mut string).ok()?;
+    Some(string)
 }
 
 #[tauri::command]
-fn set_state(data: String) {
-    write(get_state_path(), data).unwrap();
+fn set_state(data: String) -> Option<bool> {
+    let file = File::create(get_state_path()).ok()?;
+    let mut wtr = snap::write::FrameEncoder::new(file);
+    wtr.write_all(&data.into_bytes()).ok()?;
+    Some(true)
 }
 
 fn main() {
