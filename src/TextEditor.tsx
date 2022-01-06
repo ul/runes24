31fac45484ddf09b647b153e3b473e090f56d13f
Ruslan Prakapchuk <fer.obbee@gmail.com>
2022-01-06 18:43:12 +1100

    remirror -> tiptap

--- src/TextEditor.tsx
+++ src/TextEditor.tsx
@@ -1,176 +1,118 @@
-import React, { useMemo, useCallback, memo } from "react";
-import {
-  BoldExtension,
-  ItalicExtension,
-  TextColorExtension,
-  UnderlineExtension,
-} from "remirror/extensions";
-import {
-  ComponentItem,
-  FloatingToolbar,
-  ToolbarItemUnion,
-  EditorComponent,
-  Remirror,
-  useRemirror,
-  useCommands,
-  useKeymap,
-} from "@remirror/react";
-import "remirror/styles/all.css";
+import React, { memo, useMemo } from "react";
+import debounce from "lodash/debounce";
+import { Extension } from "@tiptap/core";
+import { useEditor, BubbleMenu, EditorContent } from "@tiptap/react";
+import Bold from "@tiptap/extension-bold";
+import Color from "@tiptap/extension-color";
+import Document from "@tiptap/extension-document";
+import History from "@tiptap/extension-history";
+import Italic from "@tiptap/extension-italic";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import TextStyle from "@tiptap/extension-text-style";
+import Underline from "@tiptap/extension-underline";
 import IconButton from "@mui/material/IconButton";
 import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
 import FormatColorTextIcon from "@mui/icons-material/FormatColorText";
 import FormatColorResetIcon from "@mui/icons-material/FormatColorReset";
+import Stack from "@mui/material/Stack";
 
-function BubbleMenu() {
-  const commands = useCommands();
-  const floatingToolbarItems: ToolbarItemUnion[] = useMemo(
-    () => [
-      {
-        type: ComponentItem.ToolbarGroup,
-        label: "Simple Formatting",
-        items: [
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="bold"
-                classes={{
-                  root: "text-editor-button",
-                }}
-                size="small"
-                onClick={() => commands.toggleBold()}
-              >
-                <FormatBoldIcon />
-              </IconButton>
-            ),
-          },
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="italic"
-                classes={{
-                  root: "text-editor-button",
-                }}
-                size="small"
-                onClick={() => commands.toggleItalic()}
-              >
-                <FormatItalicIcon />
-              </IconButton>
-            ),
-          },
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="underlined"
-                classes={{
-                  root: "text-editor-button",
-                }}
-                size="small"
-                onClick={() => commands.toggleUnderline()}
-              >
-                <FormatUnderlinedIcon />
-              </IconButton>
-            ),
-          },
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="red"
-                classes={{
-                  root: "text-editor-button text-editor-red-button",
-                }}
-                size="small"
-                onClick={() => commands.setTextColor("red")}
-              >
-                <FormatColorTextIcon />
-              </IconButton>
-            ),
-          },
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="green"
-                classes={{
-                  root: "text-editor-button text-editor-green-button",
-                }}
-                size="small"
-                onClick={() => commands.setTextColor("green")}
-              >
-                <FormatColorTextIcon />
-              </IconButton>
-            ),
-          },
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="blue"
-                classes={{
-                  root: "text-editor-button text-editor-blue-button",
-                }}
-                size="small"
-                onClick={() => commands.setTextColor("blue")}
-              >
-                <FormatColorTextIcon />
-              </IconButton>
-            ),
-          },
-          {
-            type: ComponentItem.ToolbarElement,
-            element: (
-              <IconButton
-                key="reset"
-                classes={{
-                  root: "text-editor-button",
-                }}
-                size="small"
-                onClick={() => commands.removeTextColor()}
-              >
-                <FormatColorResetIcon />
-              </IconButton>
-            ),
-          },
-        ],
-      },
-    ],
-    [commands]
-  );
+function Menu({ editor }) {
   return (
-    <FloatingToolbar
-      containerClass="text-editor-toolbar"
-      items={floatingToolbarItems}
-      positioner="selection"
-      placement="right"
-    />
+    <BubbleMenu
+      className="bubble-menu text-editor-toolbar"
+      tippyOptions={{ duration: 100 }}
+      editor={editor}
+    >
+      <Stack direction="row" sx={{ p: 0.25 }}>
+        <IconButton
+          key="bold"
+          classes={{
+            root: "text-editor-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().toggleBold().run()}
+        >
+          <FormatBoldIcon />
+        </IconButton>
+        <IconButton
+          key="italic"
+          classes={{
+            root: "text-editor-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+        >
+          <FormatItalicIcon />
+        </IconButton>
+
+        <IconButton
+          key="underlined"
+          classes={{
+            root: "text-editor-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+        >
+          <FormatUnderlinedIcon />
+        </IconButton>
+        <IconButton
+          key="red"
+          classes={{
+            root: "text-editor-button text-editor-red-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().setColor("red").run()}
+        >
+          <FormatColorTextIcon />
+        </IconButton>
+        <IconButton
+          key="green"
+          classes={{
+            root: "text-editor-button text-editor-green-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().setColor("green").run()}
+        >
+          <FormatColorTextIcon />
+        </IconButton>
+        <IconButton
+          key="blue"
+          classes={{
+            root: "text-editor-button text-editor-blue-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().setColor("blue").run()}
+        >
+          <FormatColorTextIcon />
+        </IconButton>
+        <IconButton
+          key="reset"
+          classes={{
+            root: "text-editor-button",
+          }}
+          size="small"
+          onClick={() => editor.chain().focus().unsetColor().run()}
+        >
+          <FormatColorResetIcon />
+        </IconButton>
+      </Stack>
+    </BubbleMenu>
   );
 }
 
-const hooks = [
-  () => {
-    const commands = useCommands();
-    const handleCopy = useCallback(() => commands.copy(), [commands]);
-    const handlePaste = useCallback(() => commands.paste(), [commands]);
-    const handleCut = useCallback(() => commands.cut(), [commands]);
-
-    // "Mod" means platform agnostic modifier key - i.e. Ctrl on Windows, or Cmd on MacOS
-    useKeymap("Mod-c", handleCopy);
-    useKeymap("Mod-v", handlePaste);
-    useKeymap("Mod-x", handleCut);
+const Clipboard = Extension.create({
+  name: "Shortcuts",
+  addKeyboardShortcuts() {
+    return {
+      "Mod-c": () => document.execCommand("copy"),
+      "Mod-v": () => document.execCommand("paste"),
+      "Mod-x": () => document.execCommand("cut"),
+    };
   },
-];
-
-const extensions = () => [
-  new BoldExtension(),
-  new ItalicExtension(),
-  new UnderlineExtension(),
-  new TextColorExtension(),
-];
+});
 
 export const TextEditor = memo(function TextEditor({
   content,
@@ -181,26 +123,36 @@ export const TextEditor = memo(function TextEditor({
   onChange: (json: any) => void;
   className?: string;
 }) {
-  const { manager, state, setState } = useRemirror({ extensions, content });
-  const update = useCallback(
-    ({ state }) => {
-      setState(state);
-      onChange(state.doc.toJSON());
-    },
-    [setState, onChange]
+  const onUpdate = useMemo(
+    () =>
+      debounce(({ editor }) => {
+        onChange(editor.state.doc.toJSON());
+      }, 250),
+    [onChange]
   );
+  const editor = useEditor({
+    extensions: [
+      Bold,
+      Clipboard,
+      Color,
+      Document,
+      History,
+      Italic,
+      Paragraph,
+      Text,
+      TextStyle,
+      Underline,
+    ],
+    content,
+    onUpdate,
+  });
+
+  if (!editor) return null;
 
   return (
-    <div className={`remirror-theme text-editor ${className ?? ""}`}>
-      <Remirror
-        manager={manager}
-        initialContent={state}
-        hooks={hooks}
-        onChange={update}
-      >
-        <EditorComponent />
-        <BubbleMenu />
-      </Remirror>
+    <div className={className}>
+      <Menu editor={editor} />
+      <EditorContent editor={editor} />
     </div>
   );
 });
