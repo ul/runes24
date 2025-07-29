7706379f6eea887bba42206cf5c3f20c987da823
Ruslan Prakapchuk <fer.obbee@gmail.com>
2025-07-29 19:58:13 +1000

    support touch

--- src/SVGDraggable.tsx
+++ src/SVGDraggable.tsx
@@ -1,5 +1,4 @@
 import { useCallback, useRef } from "react";
-import { Atom } from "./atom";
 import { Point } from "./state";
 
 const _SVGElement = document.createElementNS(
@@ -24,7 +23,10 @@ function svgLocation(node: SVGGraphicsElement, [x, y]: Point): Point {
   return [p.x, p.y];
 }
 
-function location(e: MouseEvent): Point {
+function location(e: MouseEvent | TouchEvent): Point {
+  if ("touches" in e) {
+    return [e.touches[0].clientX, e.touches[0].clientY];
+  }
   return [e.clientX, e.clientY];
 }
 
@@ -38,35 +40,48 @@ export function useSVGDraggable(
 ): (newNode: any) => void {
   const node = useRef(null);
   const drag = useCallback(
-    (e: MouseEvent) => {
+    (e: MouseEvent | TouchEvent) => {
       if (e.target) {
         setXY(svgLocation(e.target as SVGGraphicsElement, location(e)));
       }
     },
     [setXY]
   );
-  const mouseUp = useCallback((e: MouseEvent) => {
-    if (isLeftButton(e)) {
+  const handleUp = useCallback((e: MouseEvent | TouchEvent) => {
+    if ("touches" in e || isLeftButton(e as MouseEvent)) {
       window.removeEventListener("mousemove", drag);
-      window.removeEventListener("mouseup", mouseUp);
+      window.removeEventListener("mouseup", handleUp);
+      window.removeEventListener("touchmove", drag);
+      window.removeEventListener("touchend", handleUp);
       stop();
     }
   }, deps);
-  const mouseDown = useCallback((e: MouseEvent) => {
-    if (!isLeftButton(e)) return;
-    start();
-    drag(e);
-    window.addEventListener("mousemove", drag);
-    window.addEventListener("mouseup", mouseUp);
+  const handleDown = useCallback((e: MouseEvent | TouchEvent) => {
+    if ("touches" in e) {
+      e.preventDefault(); // Prevent scrolling on touch devices
+      start();
+      drag(e);
+      window.addEventListener("touchmove", drag, { passive: false });
+      window.addEventListener("touchend", handleUp);
+    } else if (isLeftButton(e as MouseEvent)) {
+      start();
+      drag(e);
+      window.addEventListener("mousemove", drag);
+      window.addEventListener("mouseup", handleUp);
+    }
   }, deps);
   const ref = useCallback((newNode) => {
     if (newNode === node.current) return;
     if (node.current) {
-      node.current.removeEventListener("mousedown", mouseDown);
+      node.current.removeEventListener("mousedown", handleDown);
+      node.current.removeEventListener("touchstart", handleDown);
     }
     node.current = newNode;
     if (node.current) {
-      node.current.addEventListener("mousedown", mouseDown);
+      node.current.addEventListener("mousedown", handleDown);
+      node.current.addEventListener("touchstart", handleDown, {
+        passive: false,
+      });
     }
   }, deps);
   return ref;
